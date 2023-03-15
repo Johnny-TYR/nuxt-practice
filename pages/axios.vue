4e959b2e45235ab28962ce80af67a5eccbb2e42c
Johnny Chang <johnny22375@gmail.com>
2023-03-16 00:26:58 +0800

    finished shiny sprites

--- pages/axios.vue
+++ pages/axios.vue
@@ -1,21 +1,23 @@
 <template lang="pug">
 #Axios
   h1.title {{ "Axios Practice" }}
-  .main-content
-    button.btn(v-if="!pokemonData" @click="GetPokeInfo") {{ "Capture Pokemon" }}
-    .pokemonCard(v-else)
-      img(v-show="false" :src="pokemonData.sprites.front_shiny")
-      p {{ pokemonData.name }}
-      p {{ pokemonData.sprites.back_default }}
+  button.btn(@click="GetPokeInfo") {{ "Get Sprite Url" }}
+  .showLink
+    .pokemonCard(v-if="pokemonData")
+      p {{ pokemonData.sprites.front_shiny }}
+      img(v-for="", :src="pokemonData.sprites.front_shiny")
     //-   pre(v-for="item of pokemonData.moves") {{ item.move.name }}
-
-
-
-    // 因為我要按按紐才會開始抓資料，所以一開始渲染沒有資料就爆掉了，但為什麼 name 沒爆，因為瀏覽器本身只能抓到第一層，
-    // 再下去一層會爆掉
-    // 2 ways to render when data is not grabbed yet
-    //-   pre {{ pokemonData?.sprites?.back_default}} or v-if 等東西都渲染完才顯示
- 
+  .main-content
+    .shinySprites
+      .spriteBox(v-for="index in 107", :key="index")
+        img.img(:src="GetShinySprites(index + 386)")
+        //- span.span {{ `# ${index + 386}` }}
+  .problems
+    h1.q-title {{ "遇到的問題！" }}
+    .text {{ notes.one }}
+    .text {{ notes.two }}
+    .solve {{ solutions.one }}
+    .solve {{ solutions.two }}
 </template>
 
 <script>
@@ -24,12 +26,23 @@ export default {
     return {
       // don't set as empty object, because it's truthy
       pokemonData: null,
+      // for getting shiny sprites
+      spriteUrl:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/",
+      // for setting notes text
+      notes: {
+        one: "1. 我們抓回來的資料要先在 data() 設變數，再將 axios 抓回來的資料用 this.dataName 存入才能使用",
+        two: "2. 因為我們是在按下 btn 才抓資料，所以一開始在還沒抓到資料的情況下渲染，畫面會爆掉。",
+      },
+      solutions: {
+        one: "解決辦法 1：pokemonData?.sprites?.back_default → 在 . 前面加問號",
+        two: "解決辦法 2：v-if 等東西都抓到了才顯示 → 記得空物件是 truthy",
+      },
     };
   },
   methods: {
     async GetPokeInfo() {
       let res = await this.$axios
-        .get("https://pokeapi.co/api/v2/pokemon/1")
+        .get(`https://pokeapi.co/api/v2/pokemon/groudon`)
         .then((res) => {
           console.log(res.data);
           this.pokemonData = res.data;
@@ -38,27 +51,77 @@ export default {
           return err;
         });
     },
+    GetShinySprites(index) {
+      return `${this.spriteUrl}${index}.png`;
+    },
   },
 };
 </script>
 
 <style lang="scss" scoped>
+* {
+  padding: 0;
+  margin: 0;
+}
 #Axios {
+  // background: black;
   .title {
     text-align: center;
     color: darkcyan;
     font-size: 50px;
     margin-top: 40px;
   }
-  .main-content {
+  .showLink {
+    margin: auto;
+    margin-top: 50px;
     background-color: tomato;
-    // width: 600px;
-    // height: 600px;
+    border-radius: 10px;
+    width: 700px;
     position: relative;
-    .btn {
-      position: absolute;
-      top: 50%;
-      left: 50%;
+  }
+  .main-content {
+    .shinySprites {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: center;
+      max-width: 700px;
+      margin: auto;
+      // outline: auto;
+      .spriteBox {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        width: 120px;
+        height: 120px;
+        background-color: white;
+        border: 5px double tomato;
+        margin: 10px;
+        border-radius: 50%;
+        img {
+          object-fit: contain;
+          width: 100%;
+        }
+      }
+    }
+  }
+  .problems {
+    margin: auto;
+    background-color: darkcyan;
+    max-width: 700px;
+    border-radius: 10px;
+    color: white;
+    padding: 40px 30px;
+    margin-top: 50px;
+    margin-bottom: 50px;
+    .text {
+      margin-top: 20px;
+    }
+    .solve {
+      background-color: black;
+      margin-top: 30px;
+      border-radius: 10px;
+      padding: 15px;
     }
   }
 }
