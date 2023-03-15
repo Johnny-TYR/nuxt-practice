b7682fd4242db82396b63f6960bbfe7cc1dd1bfa
Johnny <johnny@admindeiMac.local>
2023-03-15 18:29:58 +0800

    used collapse with add hide class

--- components/Collapse.vue
+++ components/Collapse.vue
@@ -1,58 +1,69 @@
 <template lang="pug">
 #Collapse
-  .panel 
-    .title {{ "皮卡丘" }}
-  .content {{ "This Pokemon is a psychic-type Pokemon " }}
-  .panel 
+  .panel
+    .title(@click="ToggleCollapse") {{ "皮卡丘" }}
+    .content(:class="{ hide: !isShow }") {{ "This Pokemon is a psychic-type Pokemon " }}
+  .panel
     .title {{ "水箭龜" }}
-  .content {{ "This Pokemon is a fire-type Pokemon " }}
-  .panel 
+    .content {{ "This Pokemon is a fire-type Pokemon " }}
+  .panel
     .title {{ "水箭龜" }}
-  .content {{ "This Pokemon is a bug-type Pokemon" }}
+    .content {{ "This Pokemon is a bug-type pokemon This Pokemon is a bug-type pokemonThis Pokemon is a bug-type pokemonThis Pokemon is a bug-type pokemonThis Pokemon is a bug-type pokemonThis Pokemon is a bug-type pokemon" }}
 </template>
 
-<style lang="scss">
+<script>
+export default {
+  data() {
+    return {
+      isShow: false,
+    };
+  },
+  methods: {
+    ToggleCollapse() {
+      this.isShow = !this.isShow;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
 #Collapse {
-  background-color: white;
-  box-sizing: border-box;
-  min-width: 500px;
-  max-width: 700px;
+  outline: auto;
   display: inline-flex;
   flex-direction: column;
+
   .panel {
-    min-height: 40px;
-    background-color: #d3d3d3;
     display: flex;
-    justify-content: flex-start;
-    align-items: center;
-    padding-left: 10px;
-    border-bottom: 1px solid darkgray;
-    border-right: 1px solid darkgray;
-    border-left: 1px solid darkgray;
-    &:nth-of-type(1) {
-      border-top: 1px solid darkgray;
-    }
-    // &.clicked .title {
-    //   transform: rotate(90deg);
-    //   transition: 0.5s ease-in-out;
-    //   background-color: yellow;
-    // }
+    flex-direction: column;
+    min-width: 500px;
+    max-width: 700px;
     .title {
+      background-color: lightgray;
+      padding: 10px 15px;
       &::before {
         content: "\25BE";
         margin-right: 20px;
         margin-left: 10px;
       }
+      &.clicked {
+        &::before {
+          content: "\25BE";
+          margin-right: 20px;
+          margin-left: 10px;
+          background: yellow;
+        }
+      }
+    }
+    .content {
+      background: white;
+      padding: 25px;
+      word-wrap: normal;
+      transition: all 0.5s ease-in-out;
     }
   }
-  .content {
-    min-height: 60px;
-    padding: 30px 20px;
-    word-wrap: normal;
-    background-color: white;
-    border-bottom: 1px solid darkgray;
-    border-right: 1px solid darkgray;
-    border-left: 1px solid darkgray;
+  .hide {
+    height: 0;
+    padding: 0 25px !important;
   }
 }
 </style>
