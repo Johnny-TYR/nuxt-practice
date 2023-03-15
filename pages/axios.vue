576bf3ac980fc4e395f9d29c33736c4537dfbac2
Johnny <johnny@admindeiMac.local>
2023-03-15 14:55:48 +0800

    axios is working fine and is able to render data to screen

--- pages/axios.vue
+++ pages/axios.vue
@@ -1,4 +1,29 @@
 <template lang="pug">
 #Axios
-    h1 {{"Axios Page"}}
+  h1 {{ "Axios Page" }}
+  button(@click="GetPokeInfo" v-if="pokemonData") {{ "Get Sprites" }}
+  p {{ pokemonData.name }}
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      pokemonData: {},
+    };
+  },
+  methods: {
+    async GetPokeInfo() {
+      let res = await this.$axios
+        .get("https://pokeapi.co/api/v2/pokemon/groudon")
+        .then((res) => {
+          console.log(res.data);
+          this.pokemonData = res.data;
+        })
+        .catch((err) => {
+          return err;
+        });
+    },
+  },
+};
+</script>
