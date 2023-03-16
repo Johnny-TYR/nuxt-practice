307ae997b4b5a919ade96755d7ca1feaabe50467
Johnny <johnny@admindeiMac.local>
2023-03-16 17:42:56 +0800

    step 4 arrow rotate

--- components/Collapse.vue
+++ components/Collapse.vue
@@ -4,7 +4,7 @@
     .arrow-btn(:class="{ 'rotate-arrow': isOpen }") {{ "►" }}
   .title-area {{ "This is the title" }}
   .content-area(ref="CollapseContent", :style="contentAreaStyle")
-    .content-box {{ dummyText }}
+    .content-box 
 </template>
 
 <script>
@@ -13,8 +13,6 @@ export default {
   data() {
     return {
       isOpen: false,
-      dummyText:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
     };
   },
   methods: {
@@ -51,7 +49,7 @@ export default {
     background-color: lightslategray;
     @extend .center;
     .arrow-btn {
-      transition: transform .6s ease-in-out;
+      transition: transform 0.6s ease-in-out;
     }
   }
   // top right ===========
